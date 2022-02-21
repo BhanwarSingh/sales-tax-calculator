20b5f617de32431dae9f28257b97e1c371dbd0ad
Bhanwar Singh <mailtobhanwarsingh@gmail.com>
2022-02-21 16:27:13 +0530

    Update root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "sales_tax_calculators#index"
-  get "/sales_tax_calculators", to: "sales_tax_calculators#index"
-  post "/sales_tax_calculators/create", to: "sales_tax_calculators#create"
-  get "/sales_tax_calculators/new", to: "sales_tax_calculators#new"
+  
+  # root to: "sales_tax_calculators#index"
+
+  root to: "/sales_tax_calculators/new", to: "sales_tax_calculators#new"
+  get "/sales_tax_calculators",          to: "sales_tax_calculators#index"
+  post "/sales_tax_calculators/create",  to: "sales_tax_calculators#create"
 
   #get "/articles", to: "articles#index"
 end
