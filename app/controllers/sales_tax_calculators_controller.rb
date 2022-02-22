626be967b0fd241bd2391c7f6630cfb6d1a437d0
Bhanwar Singh <mailtobhanwarsingh@gmail.com>
2022-02-22 21:17:16 +0530

    Update keywords to match description category

--- app/controllers/sales_tax_calculators_controller.rb
+++ app/controllers/sales_tax_calculators_controller.rb
@@ -1,21 +1,22 @@
 class SalesTaxCalculatorsController < ApplicationController
   include ApplicationHelper
-  def index
-    @total = session[:total_price].present? ? session[:total_price].map(&:to_f).inject(:+).round(2) : 0.00
 
-    @total_tax =  session[:sales_tax].present? ? session[:sales_tax].inject(:+).round(2) : 0.00
+  def index
+    @total     = session[:total_price].present? ? session[:total_price].map(&:to_f).inject(:+).round(2) : 0.00
+    @total_tax = session[:sales_tax].present? ? session[:sales_tax].inject(:+).round(2) : 0.00
   end
 
   def new
   end
 
   def create
-    session[:qty] = params[:qty]  
-    session[:item_description] = params[:item_description]  
-    session[:price] = params[:shelf_price]  
+    session[:qty] = params[:qty]
+    session[:item_description] = params[:item_description]
+    session[:price] = params[:shelf_price]
 
     calculate_price_with_tax
 
     redirect_to '/sales_tax_calculators'
   end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,43 +1,47 @@
 module ApplicationHelper
-	def calculate_price_with_tax 
-		arr = ['book','chocolate', 'headache']
-		p_tax = 0;
-		p_tax_imp = 0;
-		updated_price ||= []
-		sales_tax ||= []
-		if session[:qty].present?
 
-			session[:qty].each_with_index do |item, index|
+  def calculate_price_with_tax 
+    arr = ["book", "food", "medicine"]
+    p_tax = 0;
+    p_tax_imp = 0;
+    updated_price ||= []
+    sales_tax ||= []
 
-				description = session[:item_description][index].downcase
-				price = session[:price][index].to_f
-				if description.include?('imported')
-					p_tax_imp = (price * 5) / 100
-				end
-				is_exist = false
-				arr.each do |word|
-					if description.include? word
-						is_exist = true
-					end
-				end	
+    if session[:qty].present?
+      session[:qty].each_with_index do |item, index|
+        description = session[:item_description][index].downcase
+        price = session[:price][index].to_f * item.to_f
 
-				if is_exist.present?
-					updated_price << price + p_tax_imp
-					sales_tax << p_tax_imp
-				else
-					p_tax = (price * 10) / 100
-					updated_price << price + p_tax_imp + p_tax
-					sales_tax << p_tax_imp + p_tax
-				end
-			end
-		end
-		session[:total_price] = updated_price
-		session[:sales_tax] = sales_tax
-	end
+        if description.include?('imported')
+          p_tax_imp = (price * 5) / 100
+        end
 
+        is_exist = false
 
-	def currency_dropdown 
-		[['EUR', 'EUR'], ['INR', 'INR'], ['USD', 'USD']]
-	end
+        arr.each do |word|
+          if description.include?(word)
+            is_exist = true
+            break
+          end
+        end
+
+        if is_exist.present?
+          updated_price << price + p_tax_imp
+          sales_tax << p_tax_imp
+        else
+          p_tax = (price * 10) / 100
+          updated_price << price + p_tax_imp + p_tax
+          sales_tax << p_tax_imp + p_tax
+        end
+      end
+    end
+
+    session[:total_price] = updated_price
+    session[:sales_tax]   = sales_tax
+  end
+
+  def currency_dropdown
+    [['EUR', 'EUR'], ['INR', 'INR'], ['USD', 'USD']]
+  end
 
 end
